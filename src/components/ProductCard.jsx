3e7ef6a9a9e23c37a22d5b36b76aa891d8e49d9b
Emiliano Agustin Doux <emidoux@gmail.com>
2022-02-13 16:23:39 -0300

    .

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,45 +1,58 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../styles/ProductCard.css";
 import AppContext from "../context/AppContext";
 
-const ProductCard = ( {product} ) =>{
+const ProductCard = ({ product }) => {
+  const { state, addToCart } = useContext(AppContext);
 
-    const { state, addToCart } = useContext(AppContext);
+  const [animated, setAnimated] = useState(false);
 
-    const verifyIdNotExist = (id) => {
-        const idExist = state.cart.some(item => item.id === id)
-    
-        if(idExist) {
-          return verifyIdNotExist(id + 1);
+  const isAnimatedButton = () => {
+    return animated;
+  }
+
+  const animateButton = () =>{
+      setAnimated(!animated);
+  }
+
+  const verifyIdNotExist = (id) => {
+        const idExist = state.cart.some((item) => item.id === id);
+
+        if (idExist) {
+        return verifyIdNotExist(id + 1);
         } else {
-          return id;
+        return id;
         }
-      }
+    };
 
-    const handleCart = (item) =>{
-        const newId = verifyIdNotExist(item.id);
-       addToCart(item, newId);
-    }
+  const handleCart = (item) => {
+    const newId = verifyIdNotExist(item.id);
+    addToCart(item, newId);
+  };
 
-    return(
-        <div className="ProductCard-container">
-            <figure className="ProductCard-image-container">
-                <img className="ProductCard-image" src={product.attributes.image} alt={product.attributes.name} />
-            </figure>
-        <p className="ProductCard-title">
-            {product.attributes.name}
-        </p>
-        <p className="ProductCard-category">
-            {product.attributes.category}
-        </p>
-        <div className="ProductCard-footer-container">
-            <p className="ProductCard-price">
-                ${product.attributes.price}
-            </p>
-            <img className="ProductCard-addButton" src="/img/addIcon.svg" alt="Add to cart" onClick={() => handleCart(product)} />
-        </div>
+  return (
+    <div className="ProductCard-container">
+      <figure className="ProductCard-image-container">
+        <img
+          className="ProductCard-image"
+          src={product.attributes.image}
+          alt={product.attributes.name}
+        />
+      </figure>
+      <p className="ProductCard-title">{product.attributes.name}</p>
+      <p className="ProductCard-category">{product.attributes.category}</p>
+      <div className="ProductCard-footer-container">
+        <p className="ProductCard-price">${product.attributes.price}</p>
+        <img
+          className={isAnimatedButton() ? "ProductCard-addButton animate__animated animate__fadeIn" : "ProductCard-addButton"}
+          onAnimationEnd={()=> animateButton()}
+          src="/img/addIcon.svg"
+          alt="Add to cart"
+          onClick={() => (handleCart(product), animateButton())}
+        />
+      </div>
     </div>
-    );
+  );
 };
 
 export default ProductCard;
